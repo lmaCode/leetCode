1882cc011e760a03678ac11e5103fdccb8298c30
lmaCode <lipengwei_seu@163.com>
2020-08-07 00:13:02 +0800

    Partition Labels

---
+++ amazon/5-No763.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<Integer> partitionLabels(String S) {
+        int[] last = new int[26];
+		for(s.length){
+			last[S.charAt(i)-'a']=i;
+		}
+		int j, anchor;
+		List<Integer> ans ;
+		for(S.length){
+			j=Math.max(j,last[S.charAt(i)-'a']);
+			if(i==j){
+				ans.add(i-anchor+1);
+				anchor=i+1;
+			}
+		}
+		return ans;
+    }
+}

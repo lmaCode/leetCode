88919228e6f2d410dd2e5174e11bf25e37e3aa05
lmaCode <lipengwei_seu@163.com>
2020-08-07 10:03:15 +0800

    Top K Frequent Words

--- amazon/5-No763.java
+++ amazon/5-No763.java
@@ -7,7 +7,7 @@ class Solution {
 		int j, anchor;
 		List<Integer> ans ;
 		for(S.length){
-			j=Math.max(j,last[S.charAt(i)-'a']);
+			j=Math.max(j,last[S.charAt(i)-'a']);//a`b`a
 			if(i==j){
 				ans.add(i-anchor+1);
 				anchor=i+1;

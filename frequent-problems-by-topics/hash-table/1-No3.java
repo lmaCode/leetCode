cf8d7cd2ee73ddad53dbdd43314f49b10e61d46a
lmaCode <lipengwei_seu@163.com>
2020-08-11 10:06:23 +0800

    Verifying an Alien Dictionary

--- frequent-problems-by-topics/hash-table/1-No3.java
+++ frequent-problems-by-topics/hash-table/1-No3.java
@@ -5,7 +5,7 @@ public class Solution {// O(n) O(min(m,n))
         // try to extend the range [i, j]
         for (int j = 0, i = 0; j < n; j++) {
             if (map.containsKey(s.charAt(j))) {
-                i = Math.max(map.get(s.charAt(j)), i);
+                i = Math.max(map.get(s.charAt(j)), i);//
             }
             ans = Math.max(ans, j - i + 1);
             map.put(s.charAt(j), j + 1);
